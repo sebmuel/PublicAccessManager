a7e25633a7963510b1eff2a37025257013e2db25
sebmuel <mueller.sebastian117@protonmail.com>
2025-02-27 17:02:46 +0100

    Working on context

---
+++ PublicAccessManager.Backoffice/Client/src/elements/publicAccessEntityAction.ts
@@ -0,0 +1,7 @@
+﻿import { UmbLitElement } from "@umbraco-cms/backoffice/lit-element";
+
+
+
+export class PublicAccessEntityAction extends UmbLitElement {
+
+}
---
+++ PublicAccessManager.Backoffice/Client/src/elements/publicAccessEntityContext.ts
@@ -0,0 +1,13 @@
+﻿import {UmbControllerBase} from "@umbraco-cms/backoffice/class-api";
+import {UmbContextToken} from "@umbraco-cms/backoffice/context-api";
+
+
+export const UMB_PUBLIC_ACCESS_ENTITY_CONTEXT = new UmbContextToken<UmbPublicAccessEntityContext>
+(
+    "PublicAccessEntityContext.Alias",
+    "PublicAccessEntityApi.Alias"
+);
+
+export default class UmbPublicAccessEntityContext extends UmbControllerBase {
+
+}
