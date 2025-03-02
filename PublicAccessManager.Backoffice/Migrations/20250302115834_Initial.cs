d0791b5f8f3fa7c0b510de7b080d1a24bc973fd8
sebmuel <mueller.sebastian117@protonmail.com>
2025-03-02 13:05:52 +0100

    Create initial migration

---
+++ PublicAccessManager.Backoffice/Migrations/20250302115834_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PublicAccessManager.Backoffice.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "defaultPublicAccessPages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ErrorPageId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    LoginPageId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UpdatedAtUtc = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_defaultPublicAccessPages", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "defaultPublicAccessPages");
+        }
+    }
+}
